4e974b0e16f4f5a5a816a0303742e4b7f2777cf2
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2013-03-24 22:01:26 -0300

    Changing project url and license.

--- setup.py
+++ setup.py
@@ -9,9 +9,8 @@ setup(
     author = 'Victor Oliveira da Silva',
     author_email = 'victor_o_silva@hotmail.com',
     packages = ['db_file_storage', 'db_file_storage.test'],
-    scripts = [],
-    url = 'http://pypi.python.org/pypi/DjangoDBFileStorage/',
-    license = 'LICENSE.txt',
+    url = 'http://pypi.python.org/pypi/django-db-file-storage/',
+    license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
     description = "Custom FILE_STORAGE for Django. Saves model's filefields in your database instead of your file system.",
     long_description = open('README.txt').read(),
     install_requires=[
