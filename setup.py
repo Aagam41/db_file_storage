6e1a9232851a813c1cc1e8363081c9e7be0f91d8
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2015-02-14 02:37:50 -0200

    setup-related adjustments

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from distutils.core import setup
 
 setup(
     name = 'django-db-file-storage',
-    version = '0.2.1',
+    version = '0.2.2',
     author = 'Victor Oliveira da Silva',
     author_email = 'victor_o_silva@hotmail.com',
     packages = ['db_file_storage', 'db_file_storage.test'],
     url = 'http://pypi.python.org/pypi/django-db-file-storage/',
     license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
     description = "Custom FILE_STORAGE for Django. Saves model's filefields in your database instead of your file system.",
-    long_description = open('README.txt').read(),
+    long_description = open('docs/index.rst').read(),
     install_requires=[
         "Django",
     ],
