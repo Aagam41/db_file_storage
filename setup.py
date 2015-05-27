cc7da792ad1f4f1675e067a58edce9be812c896f
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2015-05-27 12:54:07 -0300

    Adding repo URLs to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     author='Victor Oliveira da Silva',
     author_email='victor_o_silva@hotmail.com',
     packages=['db_file_storage'],
-    url='http://pypi.python.org/pypi/django-db-file-storage/',
+    url='https://github.com/victor-o-silva/db_file_storage',
+    download_url='https://github.com/victor-o-silva/db_file_storage/tarball/0.2.9',
     license='GNU GPL v3',
     description="Custom FILE_STORAGE for Django. Saves model's filefields"
         "in your database instead of your file system.",
