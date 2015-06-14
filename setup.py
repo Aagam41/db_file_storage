f07c1caed825f6728e053cc3f105f1d446da2987
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2015-06-14 01:39:25 -0300

    updating version number

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ readme_file = open('README.rst')
 
 setup(
     name='django-db-file-storage',
-    version='0.2.10',
+    version='0.2.11',
     author='Victor Oliveira da Silva',
     author_email='victor_o_silva@hotmail.com',
     packages=['db_file_storage'],
     url='https://github.com/victor-o-silva/db_file_storage',
     download_url='https://github.com/victor-o-silva/db_file_storage'
-                 '/tarball/0.2.10',
+                 '/tarball/0.2.11',
     license='GNU GPL v3',
     description="Custom FILE_STORAGE for Django. Saves model's filefields "
                 "in your database instead of your file system.",
