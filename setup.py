69281da6f69bbdc5cfb832efa0b0c1b7810eb262
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2015-07-22 11:48:46 -0300

    Mark as compatible for python 2.7, 3.3 and 3.4
    Add `classifiers` parameter to `setup` function call in `setup.py` file.

--- setup.py
+++ setup.py
@@ -20,6 +20,16 @@ setup(
     install_requires=[
         "Django",
     ],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
 
 readme_file.close()
