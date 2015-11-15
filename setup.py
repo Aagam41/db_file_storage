483cabf743b890bb68b4ed2c0b786bd9c6bcaa16
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2015-11-15 12:47:50 -0200

    Updating minor version number

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ readme_file = open('README.rst')
 
 setup(
     name='django-db-file-storage',
-    version='0.3.1',
+    version='0.3.2',
     author='Victor Oliveira da Silva',
     author_email='victor_o_silva@hotmail.com',
     packages=['db_file_storage'],
     url='https://github.com/victor-o-silva/db_file_storage',
     download_url='https://github.com/victor-o-silva/db_file_storage'
-                 '/tarball/0.3.1',
+                 '/tarball/0.3.2',
     license='GNU GPL v3',
     description="Custom FILE_STORAGE for Django. Saves files "
                 "in your database instead of your file system.",
