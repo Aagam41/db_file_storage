e308365bbd5fcc1653eaed9af06481dcad66c545
Victor Oliveira da Silva <victor_o_silva@hotmail.com>
2017-12-05 20:53:56 -0200

    Fix download_url in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     },
     url='https://github.com/victor-o-silva/db_file_storage',
     download_url='https://github.com/victor-o-silva/db_file_storage'
-                 '/tarball/0.4.4',
+                 '/tarball/0.4.5',
     description="Custom FILE_STORAGE for Django. Saves files "
                 "in your database instead of your file system.",
     long_description=readme_file.read(),
