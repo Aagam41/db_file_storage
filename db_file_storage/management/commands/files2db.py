dadb948b83a9479d28076fec784a8de8f4b89de6
Mirek Zvolský <zvolsky@seznam.cz>
2018-02-21 21:35:39 +0100

    files2db near to ready

--- db_file_storage/management/commands/files2db.py
+++ db_file_storage/management/commands/files2db.py
@@ -4,6 +4,8 @@ import re
 
 from django.apps import apps
 from django.db import models
+from django.conf import settings
+from django.core.files import File
 from django.core.management.base import BaseCommand
 
 
@@ -14,6 +16,15 @@ DB_PATTERN = re.compile(r'^\w+\.\w+/bytes/filename/mimetype$')
 
 class Command(BaseCommand):
     help = _("Copy separate media files from file system into database after the migration to db_file_storage.")
+
+    def __init__(self):
+        super(Command, self).__init__()
+        try:
+            self.MEDIA_ROOT = getattr(settings, 'MMEDIA_ROOT')
+        except AttributeError:
+            self.stderr.write(
+                    'Please configure MEDIA_ROOT in your settings. '
+                    'Otherwise files in standard file storage cannot be found.')
 
     def add_arguments(self, parser):
         parser.formatter_class = argparse.RawTextHelpFormatter
@@ -27,27 +38,40 @@ class Command(BaseCommand):
         for app, tbl, model, fld in self.get_media_fields():
             if re.match(DB_PATTERN, fld.upload_to):
                 kwargs = {
-                    '{0}__gt'.format(fld.name): '',
+                    '{0}__exact'.format(fld.name): '',
                 }
-                qs_files = model.objects.select_for_update().filter(**kwargs).only(
+                media_files = model.objects.select_for_update().exclude(**kwargs).only(
                         model._meta.pk.name, fld.name)
 
-                cnt_format_db = 0
-                for qs_file in qs_files:
-                    field_file = getattr(qs_file, fld.name)
-                    if re.match(DB_PATTERN, os.path.dirname(field_file.name)):
-                        cnt_format_db += 1
-                self.report(app, tbl, fld, 'db', len(qs_files), cnt_format_db)
+                cnt_non_db = cnt_format_unknown = 0
+                for media_file in media_files:
+                    field_file = getattr(media_file, fld.name)
+                    if not re.match(DB_PATTERN, os.path.dirname(field_file.name)):
+                        cnt_non_db += 1
+                        maybe_file = os.path.join(self.MEDIA_ROOT, field_file.name)
+                        if os.path.isfile(maybe_file):
+                            self.cp(field_file, maybe_file)
+                        else:
+                            cnt_format_unknown += 1
+                self.report(app, tbl, fld, 'db', len(media_files), cnt_non_db, cnt_format_unknown)
             else:
                 self.report(app, tbl, fld, 'other')
 
-    def report(self, app, tbl, fld, storage, cntfiles=None, cnt_format_db=0):
+    @staticmethod
+    def cp(field_file, media_file):
+        new_name = os.path.basename(media_file)
+        with open(media_file, 'rb') as f:
+            field_file.save(new_name, File(f))
+
+    def report(self, app, tbl, fld, storage, cntfiles=None, cnt_non_db=0, cnt_format_unknown=0):
         if cntfiles is not None:
-            storage = '%s - %s file(s) - %s std format - %s unknown - %s db format' % (
-                    storage, cntfiles, cntfiles - cnt_format_db, 0, cnt_format_db)
+            storage = '%s storage - contains %s file(s): %s std format - %s%s db format' % (
+                    storage, cntfiles, cnt_non_db - cnt_format_unknown,
+                    '%s unknown - ' if cnt_format_unknown else '', cntfiles - cnt_non_db)
         self.stdout.write('%s %s %s - %s' % (app.label, tbl, fld.name, storage))
 
-    def get_media_fields(self):
+    @staticmethod
+    def get_media_fields():
         for app in apps.get_app_configs():
             for tbl, model in app.models.items():
                 for fld in model._meta.get_fields():
